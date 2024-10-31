e0477c93c4ea0b9c02cee83c4696274a2c6af56d
Brandon Robertiello <77466123+Br4nd0nR@users.noreply.github.com>
2024-10-31 14:45:35 -0500

    Landing Mobile

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './LandingPage.css'; // You’ll add your CSS here
 
+// Desktop Assets
 import HacklahomaTitle from '../assets/S1_HacklahomaTitle.png';
 import HacklahomaTitle2025 from '../assets/S1_2025Title.png';
 import HacklahomaSubtitle from '../assets/S1_HacklahomaSubtitle.png';
@@ -10,6 +11,17 @@ import Clouds from '../assets/S1_Clouds.png';
 
 import MLHBanner from '../assets/S1_MLH-Banner.png';
 import ScissorTail from "../assets/S1_ScissorTailLogo.png";
+
+// Mobile Assets
+import MobileHacklahomaTitle from "../assets/MS1_Title.png";
+import MobileHacklahomaTitle2025 from "../assets/MS1_2025.png";
+import MobileHacklahomaSubtitle from "../assets/MS1_Subtitle.png";
+
+import MobileCity from "../assets/MS1_City.png";
+import MobileClouds from "../assets/MS1_Clouds.png";
+
+import MobileMLHBanner from "../assets/MS1_MLHBanner.png";
+import MobileScissorTail from "../assets/MS1_Logo.png";
 
 const LandingPage = () => {
 
@@ -34,6 +46,17 @@ const LandingPage = () => {
 
             {/* Mobile Layout */}
             <div className="landing-page-mobile">
+
+                <img className="title HacklahomaTitle" src={MobileHacklahomaTitle} alt="Hacklahoma Title" />
+                <img className="title HacklahomaTitle2025" src={MobileHacklahomaTitle2025} alt="Hacklahoma Title 2025" />
+
+                <img className="subtitle HacklahomaSubtitle" src={MobileHacklahomaSubtitle} alt="Hacklahoma Subtitle" />
+
+                <img className="city City" src={MobileCity} alt="City" />
+                <img className="city Clouds" src={MobileClouds} alt="Clouds" />
+
+                <img className="mlhBanner" src={MobileMLHBanner} alt="MLHBanner" />
+                <img className="logo ScissorTail" src={MobileScissorTail} alt="ScissorTail Logo" />
 
             </div>
 
