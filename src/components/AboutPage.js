291cdee46013100dd283be7a89ae66556731afb0
Brandon Robertiello <77466123+Br4nd0nR@users.noreply.github.com>
2024-10-31 14:52:39 -0500

    About Mobile

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './AboutPage.css';
 
+// Desktop Assets
 import TitleText from '../assets/S2_TitleText.png';
 import TitleHighlight from '../assets/S2_TitleHighlight.png';
 import BodyText from '../assets/S2_BodyText.png';
 
 import ComicBook from '../assets/S2_ComicBook.png';
+
+//Mobile Assets
+import MobileTitleText from '../assets/MS2_TitleText.png';
+import MobileTitleHighlight from '../assets/MS2_TitleHighlight.png';
+import MobileBodyText from '../assets/MS2_BodyText.png';
+
+import MobileComicBook from '../assets/MS2_ComicBook.png';
 
 const AboutPage = () => {
 
@@ -25,6 +33,12 @@ const AboutPage = () => {
 
             {/* Mobile Layout */}
             <div className="about-page-mobile">
+
+                <img className="TitleHighlight" src={MobileTitleHighlight} alt="TitleHighlight" />
+                <img className="TitleText" src={MobileTitleText} alt="TitleText" />
+                <img className="BodyText" src={MobileBodyText} alt="BodyText" />
+
+                <img className="ComicBook" src={MobileComicBook} alt="ComicBook" />
 
             </div>
 
